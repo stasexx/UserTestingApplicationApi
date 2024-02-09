e388758fee703763aa11fe694f4b484f4d636c39
stasex <stasek.molchan@gmail.com>
2024-02-09 22:14:49 +0200

    add new entities

---
+++ Domain/Entities/Question.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+
+public class Question
+{
+    public Guid Id { get; set; }
+    
+    public string Tittle { get; set; }
+    
+    public string[] Options { get; set; }
+    
+    public string Answer { get; set; }
+    
+    public Test Test { get; set; }
+    
+    public Guid TestId { get; set; }
+}
